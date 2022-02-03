4f03dfab1bd58582202a0ee18f33017053a18f7a
AyaGElsaadany <ayah.g.elsaadany@gmail.com>
2022-02-03 15:19:07 +0200

    'l10'

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart: [],
             premium: true
         }
     },
-    methods: {}
+    methods: {
+        updateCart(id){
+            this.cart.push(id)
+        },
+        remove(id){
+            for( var i = 0; i < this.cart.length; i++){ 
+                if ( this.cart[i] === id) { 
+                    this.cart.splice(i, 1); 
+                }
+                break;
+            }
+            
+            // const index = this.cart.indexOf(id)
+            // console.log(index)
+            // if (index > -1) {
+            //     this.cart.splice(index, 1)
+            // }
+        }
+    }
 })
